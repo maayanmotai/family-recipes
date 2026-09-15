c7d83c553c2971950da3ee1da3b09d9df301f222
Maayan Motai <maayan.motai@gmail.com>
2026-09-15 20:26:39 +0300

    1

---
+++ temp_test.js
@@ -0,0 +1,4 @@
+
+  const SUPABASE_URL = "https://xtaeopklnjxvvnpqhgyx.supabase.co";
+  const SUPABASE_KEY = "sb_publishable_9-oJ5ZS2rxk65o9BUtiwoA_6rYxa-qz";
+  const supabaseClient = window.supabase ? window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY) : null;
